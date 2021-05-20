70a466bdb1b81eef1cb5042281ae2424be390a48
Nselb <70535124+Nselb@users.noreply.github.com>
2021-05-20 18:54:07 -0500

    A

--- Proyecto Definitivo/Assets/Scripts/PlayerController.cs
+++ Proyecto Definitivo/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
     private Vector3 _movement;
     public int vida = 100;
+    public Image farmingImage;
     void Start()
     {
 
@@ -23,21 +25,23 @@ public class PlayerController : MonoBehaviour
         transform.Translate(_movement * speed * Time.deltaTime, Space.Self);
         if (Keyboard.current.eKey.wasPressedThisFrame)
         {
+            farmingImage.fillAmount = 0;
             StartCoroutine("Collect");
         }
         if (!Keyboard.current.eKey.isPressed)
         {
+            farmingImage.fillAmount = 0;
             StopCoroutine("Collect");
         }
     }
 
     IEnumerator Collect()
     {
-        while(vida > 0)
+        while (vida > 0)
         {
-            Debug.Log("Farming...");
-            yield return new WaitForSeconds(1f);
-            vida-=10;
+            yield return new WaitForSeconds(.28f);
+            vida -= 10;
+            farmingImage.fillAmount = vida / 100f;
         }
-    } 
+    }
 }
